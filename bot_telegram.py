3a631ee629c12c41e95a224725a47513ec86a481
Edybrown <gozalaventa@gmail.com>
2025-01-08 21:40:10 -0500

    bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -169,16 +169,16 @@ async def button(update: Update, context: CallbackContext):
 # logica del bot ----------------------------------------------------------------------------------------------------
 
 def get_time_to_close(temporalidad):
-    current_time = datetime.now()
-    if temporalidad == '15m':
-        next_close_time = current_time.replace(second=0, microsecond=0) + timedelta(minutes=15)
-    elif temporalidad == '1h':
-        next_close_time = current_time.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
-    elif temporalidad == '4h':
-        next_close_time = current_time.replace(hour=(current_time.hour // 4) * 4, minute=0, second=0, microsecond=0) + timedelta(hours=4)
-    elif temporalidad == '1d':
-        next_close_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
-    return next_close_time
+  current_time = datetime.now()
+  if temporalidad == '15m':
+    next_close_time = current_time.replace(second=0, microsecond=0) + timedelta(minutes=15)
+  elif temporalidad == '1h':
+    next_close_time = current_time.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+  elif temporalidad == '4h':
+    next_close_time = current_time.replace(hour=(current_time.hour // 4) * 4, minute=0, second=0, microsecond=0) + timedelta(hours=4)
+  elif temporalidad == '1d':
+    next_close_time = current_time.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+  return next_close_time
 
 # Función para verificar si el archivo está actualizado
 def is_report_updated(temporalidad):
