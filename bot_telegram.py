86167fc2013e395953235d3294c7da041feff492
Edybrown <gozalaventa@gmail.com>
2025-01-10 17:54:09 -0500

    bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -163,10 +163,10 @@ async def send_reports_to_all_users(bot):
 def handle_candle_closure(bot):
     while True:
         print("[INFO] Iniciando ciclo de cierre de vela.")
-        await check_and_send_reports(bot)
+        check_and_send_reports(bot)  # Llama a la función de forma síncrona
         time_to_close = get_time_to_close()
         print(f"[INFO] Esperando {time_to_close} segundos hasta el próximo cierre de vela.")
-        await asyncio.sleep(time_to_close)
+        time.sleep(time_to_close)  # Espera síncronamente
 
 
 
