aecd3b9dfc0d580c9403f6612decb18c00373a7a
Edybrown <gozalaventa@gmail.com>
2025-01-11 11:41:43 -0500

    bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,8 +7,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Call
 from datetime import datetime, timedelta
 
 reports_dir = "Analisis_trading"
-chat_id ={}
-suscripciones =[]
+
 
 load_dotenv()
 bot_token = os.getenv('TELEGRAM_TOKEN')
