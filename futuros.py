9896ba13c2c1de98a4c3aee0ebb0ed67aedf2c82
Edybrown <gozalaventa@gmail.com>
2025-01-23 22:54:27 -0500

    futuros.py

--- futuros.py
+++ futuros.py
@@ -19,7 +19,7 @@ OUTPUT_FOLDER = "datos_futuros"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 # Fechas para obtener 2000 velas por temporalidad
-VELAS = 1500
+VELAS = 1000
 TEMPORALIDAD_SEGUNDOS = {
     "1hour": 3600,
     "4hour": 14400,
